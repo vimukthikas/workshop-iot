592c2a138dff7f94a8c88588470d64d2b5cea9a7
Martian <kasunvj@gmail.com>
2024-11-15 14:39:25 +0530

    relocate server to folder added activity 2

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 This is created for the workshop on Nov 2024
+
+Open Postman and create a new request.
+Set the request type to POST.
+Enter the URL: http://localhost:3000/sensor-data.
+In the Body tab, select raw and set the format to JSON.
+Enter the sensor data in JSON format:
+{
+    "value": 75.3
+}
+
---
+++ activity2_sound/arduino/readsensor/readsensor.ino
@@ -0,0 +1,17 @@
+	
+int sound_sensor = A2; //assign to pin A2
+ 
+void setup() 
+{
+  Serial.begin(9600); //begin Serial Communication
+}
+ 
+void loop()
+{
+  int soundValue = 0; //create variable to store many different readings
+  //for (int i = 0; i < 32; i++) //create a for loop to read 
+  soundValue = analogRead(sound_sensor);   //read the sound sensor
+  Serial.println(soundValue); //print the value of sound sensor
+
+  delay(50); //a shorter delay between readings
+}
